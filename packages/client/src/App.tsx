8074bb952e4fecfe09647446bede4ffcd1a7341c
mattgeroulo <mattgeroulo@yahoo.com>
2026-01-18 16:53:29 -0500

    Added shadcn compatibility

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,13 +1,15 @@
 import {useState, useEffect} from 'react';
-
+import { Button } from "@/components/ui/button"
 
 function App(){
   const [message,setMessage] = useState(" ")
   useEffect(()=>{
     fetch('/api/hello').then(res=>res.json()).then(data=>setMessage(data.message))
   },[])
-  return <p className="font-bold p-4 text-3xl">{message}</p>
-  
+  return( <div className="p-4">
+    <p className="font-bold p-4 text-3xl">{message}</p>
+    <Button variant="destructive">Button</Button>
+  </div>)
 };
 
 export default App
